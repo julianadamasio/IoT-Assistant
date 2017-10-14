dd372c4997a4e3cb56b5cdca9e9bf81769c1cf47
Rafael <leafarmd@gmail.com>
2017-10-14 17:27:51 -0300

    update

--- index.js
+++ index.js
@@ -56,17 +56,17 @@ restService.post('/echo', function(req, res) {
           sendResponse("<speak>There will be an event called Entrepreneurship in the academic world, on October 16, 2017, at 7:00 pm in the auditorium of building 15, second floor. There will be an event called Legislation and Philosophy, on October 16, 2017, at 4:00 p.m. in Room 303 of Building 11 on the third floor.</speak>");
     }else
 
-    //BIBLIOTECA
-    if(message.indexOf(title) > -1) {
-        if(message.indexOf(book1) > -1) { //book1 = "designing interfaces";
-          sendResponse("<speak>The book designing interfaces is available for lease for 7 days. Its location is on the 3rd floor, shelf number 16.</speak>");
-        }else if(message.indexOf(book2) > -1) { //book2 = "scrum";
-          sendResponse("<speak>This book is not available. Borrowed until Oct 16, 2017 22:50.</speak>");
-    }else
-
-    if(message.indexOf(keyWord) > -1) {
-      sendResponse("<speak>The books: Software development rhythms, Software development failures, Running an agile software development project and Using aspect-oriented programming for trustworthy software development are the results returned from your search.</speak>");
-    }
+    // //BIBLIOTECA
+    // if(message.indexOf(title) > -1) {
+    //     if(message.indexOf(book1) > -1) { //book1 = "designing interfaces";
+    //       sendResponse("<speak>The book designing interfaces is available for lease for 7 days. Its location is on the 3rd floor, shelf number 16.</speak>");
+    //     }else if(message.indexOf(book2) > -1) { //book2 = "scrum";
+    //       sendResponse("<speak>This book is not available. Borrowed until Oct 16, 2017 22:50.</speak>");
+    // }else
+    //
+    // if(message.indexOf(keyWord) > -1) {
+    //   sendResponse("<speak>The books: Software development rhythms, Software development failures, Running an agile software development project and Using aspect-oriented programming for trustworthy software development are the results returned from your search.</speak>");
+    // }
 
     //Função de envio de mensagem
     function sendResponse(msg) {
